4644f46773ec75881407c70d283b81b582f2882f
duyvd <duy.vd176936@gmail.com>
2021-08-18 13:44:25 +0700

    beta vêr

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const mqtt = require('mqtt')
 const fs = require('fs')
-const client = mqtt.connect('mqtt://127.0.0.1')
-const TOPIC = 'channel1'
+let PouchDB = require('pouchdb'); 
+const client = mqtt.connect('mqtt://127.0.0.1') // Change it to local broker
+const TOPIC = 'channel1'    // TOPIC for Hcv2 Edge <=> Homekit
+const databasePath = '/mnt/d/cvert/db/db/resource/node-edge-server/edgeDatabase' // replace it by db hcv2 path
 
 //Connect to broker and subscribe
 client.on('connect',function(){
@@ -16,6 +18,23 @@ function checkcmd(msg){
     else return -1
 }
 
+function gethash(devid){
+    return new Promise((resolve,reject)=>{
+        const db = new PouchDB(databasePath, { adapter: 'leveldb', db: require('leveldown') });
+                db.get('devices', function(err, doc) {
+                    if (err) {
+                       reject(err);
+                    } else {
+                        let find = doc.devices.find(function(e){
+                            return e.devid == devid
+                        })
+                        if(find == null) reject('No devid found');
+                        else resolve(find)
+                 };
+            })
+    })
+    
+}
 
 //Get message and handle
 client.on('message',function(topic,payload){
@@ -45,7 +64,18 @@ try
 
                 //handle for status message
                 
-
+                    gethash(msg.objects[0].data[0].devid)
+                        .then(function(resolve){
+                            msg.objects[0].data[0].hash = resolve.hash
+                            msg.objects[0].data[0].type = resolve.type
+                            msg.source = fs.readFileSync('/sys/class/net/eth0/address', 'utf8').replace(/\n/g, '') // global.macadrress
+                            let message = JSON.stringify(msg)
+                            console.log('Publish message to Homekit => '+ message)
+                            client.publish(TOPIC,message)
+                        })
+                        .catch(error => console.log(error))
+            
+                
                 break
             }
             case 3:
